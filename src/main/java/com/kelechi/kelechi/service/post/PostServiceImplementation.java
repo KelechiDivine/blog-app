211ed1b76a979abf09fc9eceec6b6349b4779937
Kelechi Divine <74909446+KelechiDivine@users.noreply.github.com>
2021-05-31 10:10:52 +0100

    new commit

--- src/main/java/com/kelechi/kelechi/service/post/PostServiceImplementation.java
+++ src/main/java/com/kelechi/kelechi/service/post/PostServiceImplementation.java
@@ -4,11 +4,14 @@ import com.kelechi.kelechi.data.models.Comment;
 import com.kelechi.kelechi.data.models.Post;
 import com.kelechi.kelechi.data.repository.PostRepository;
 import com.kelechi.kelechi.web.dto.PostDto;
+import lombok.extern.slf4j.Slf4j;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Slf4j
 @Service
 public class PostServiceImplementation implements PostService{
 	
@@ -17,7 +20,14 @@ public class PostServiceImplementation implements PostService{
 	
 	@Override
 	public Post savePost(PostDto postDto) {
-		return null;
+		Post post =  new Post();
+		
+		ModelMapper modelMapper = new ModelMapper();
+		
+		modelMapper.map(postDto, post);
+		log.info("Post object after mapping --> {}", post);
+		
+		return postRepository.save(post);
 	}
 	
 	@Override
--- src/test/java/com/kelechi/kelechi/service/post/PostServiceImplementationTest.java
+++ src/test/java/com/kelechi/kelechi/service/post/PostServiceImplementationTest.java
@@ -1,26 +1,43 @@
 package com.kelechi.kelechi.service.post;
 
+import com.kelechi.kelechi.data.models.Post;
 import com.kelechi.kelechi.data.repository.PostRepository;
+import com.kelechi.kelechi.web.dto.PostDto;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
+import static org.mockito.Mockito.*;
+
 class PostServiceImplementationTest {
-	
 	
 	
 	@Mock
 	PostRepository postRepository;
 	
 	@InjectMocks
-	PostService postServiceImplementation = new PostServiceImplementation();
+	PostServiceImplementation postServiceImplementation = new PostServiceImplementation();
 	
-	
+	Post testPost;
 	
 	@BeforeEach
 	void setUp() {
 		
+		testPost = new Post();
+		
 		MockitoAnnotations.openMocks(this);
 	}
+	
+	@Test
+	public void whenTheSaveMethodIsCalled_thenRepositoryIsCalledOnceTest() {
+		when(postServiceImplementation.savePost(new PostDto())).thenReturn(testPost);
+		
+		postServiceImplementation.savePost(new PostDto());
+		
+		
+		verify(postRepository, times(1)).save(testPost);
+
 	}
+}
